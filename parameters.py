cdcb4c4f3653b32a2ff19c2698aefe12ba7f7dee
deepliver-tesla-v100 <lightingsoon@163.com>
2019-02-01 11:52:40 +0800

    v100

--- parameters.py
+++ parameters.py
@@ -1,3 +1,4 @@
+import os
 mo_name = 'model.h5'
 we_name = 'weight.h5'
 plot_name = 'Unet3d.pdf'
@@ -5,3 +6,5 @@ img_rows, img_cols = None, None
 train_path = '../LITS/train'
 validate_path = '../LITS/val'
 logs_path = './logs'
+if not os.path.isdir(logs_path):
+    os.mkdir(logs_path)
